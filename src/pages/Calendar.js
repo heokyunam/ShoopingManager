dd9726ac5da5c3bc0c13cfa5a4a70aa03af88418
heokyunam <heokyunam@naver.com>
2021-02-22 21:35:22 +0900

    캘린더 페이지 1차 CSS 작업

---
+++ src/pages/Calendar.js
@@ -0,0 +1,244 @@
+import './Calendar.scss';
+
+export const CalendarPage = () => {
+    return (
+        <div className="calendar-page">
+            <div className="month">
+                Febrary
+            </div>
+            <div className="calendar">
+                <table>
+                    <colgroup>
+                        <col width="14%"/>
+                        <col width="14%"/>
+                        <col width="14%"/>
+                        <col width="14%"/>
+                        <col width="14%"/>
+                        <col width="14%"/>
+                        <col width="14%"/>
+                    </colgroup>
+                    <thead>
+                        <tr>
+                            <th>S</th>
+                            <th>M</th>
+                            <th>T</th>
+                            <th>W</th>
+                            <th>T</th>
+                            <th>F</th>
+                            <th>S</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td className="active">
+                                <div className="day">1</div>
+                                <div className="markers">
+                                    <div className="red marker"></div>
+                                    <div className="blue marker"></div>
+                                </div>
+                            </td>
+                            <td>
+                                <div className="day">2</div>
+                                <div className="markers">
+                                    <div className="red marker"></div>
+                                    <div className="blue marker"></div>
+                                </div>
+                            </td>
+                            <td>
+                                <div className="day">3</div>
+                                <div className="markers">
+                                    <div className="red marker"></div>
+                                    <div className="blue marker"></div>
+                                </div>
+                            </td>
+                            <td>
+                                <div className="day">4</div>
+                                <div className="markers">
+                                    <div className="red marker"></div>
+                                    <div className="blue marker"></div>
+                                </div>
+                            </td>
+                            <td>
+                                <div className="day">5</div>
+                                <div className="markers">
+                                    <div className="red marker"></div>
+                                    <div className="blue marker"></div>
+                                </div>
+                            </td>
+                            <td>
+                                <div className="day">6</div>
+                                <div className="markers">
+                                    <div className="red marker"></div>
+                                    <div className="blue marker"></div>
+                                </div>
+                            </td>
+                            <td>
+                                <div className="day">7</div>
+                                <div className="markers">
+                                    <div className="red marker"></div>
+                                    <div className="blue marker"></div>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div className="day">1</div>
+                                <div className="markers">
+                                    <div className="red marker"></div>
+                                    <div className="blue marker"></div>
+                                </div>
+                            </td>
+                            <td>
+                                <div className="day">2</div>
+                                <div className="markers">
+                                    <div className="red marker"></div>
+                                    <div className="blue marker"></div>
+                                </div>
+                            </td>
+                            <td>
+                                <div className="day">3</div>
+                                <div className="markers">
+                                    <div className="red marker"></div>
+                                    <div className="blue marker"></div>
+                                </div>
+                            </td>
+                            <td>
+                                <div className="day">4</div>
+                                <div className="markers">
+                                    <div className="red marker"></div>
+                                    <div className="blue marker"></div>
+                                </div>
+                            </td>
+                            <td>
+                                <div className="day">5</div>
+                                <div className="markers">
+                                    <div className="red marker"></div>
+                                    <div className="blue marker"></div>
+                                </div>
+                            </td>
+                            <td>
+                                <div className="day">6</div>
+                                <div className="markers">
+                                    <div className="red marker"></div>
+                                    <div className="blue marker"></div>
+                                </div>
+                            </td>
+                            <td>
+                                <div className="day">7</div>
+                                <div className="markers">
+                                    <div className="red marker"></div>
+                                    <div className="blue marker"></div>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div className="day">1</div>
+                                <div className="markers">
+                                    <div className="red marker"></div>
+                                    <div className="blue marker"></div>
+                                </div>
+                            </td>
+                            <td>
+                                <div className="day">2</div>
+                                <div className="markers">
+                                    <div className="red marker"></div>
+                                    <div className="blue marker"></div>
+                                </div>
+                            </td>
+                            <td>
+                                <div className="day">3</div>
+                                <div className="markers">
+                                    <div className="red marker"></div>
+                                    <div className="blue marker"></div>
+                                </div>
+                            </td>
+                            <td>
+                                <div className="day">4</div>
+                                <div className="markers">
+                                    <div className="red marker"></div>
+                                    <div className="blue marker"></div>
+                                </div>
+                            </td>
+                            <td>
+                                <div className="day">5</div>
+                                <div className="markers">
+                                    <div className="red marker"></div>
+                                    <div className="blue marker"></div>
+                                </div>
+                            </td>
+                            <td>
+                                <div className="day">6</div>
+                                <div className="markers">
+                                    <div className="red marker"></div>
+                                    <div className="blue marker"></div>
+                                </div>
+                            </td>
+                            <td>
+                                <div className="day">7</div>
+                                <div className="markers">
+                                    <div className="red marker"></div>
+                                    <div className="blue marker"></div>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div className="day">1</div>
+                                <div className="markers">
+                                    <div className="red marker"></div>
+                                    <div className="blue marker"></div>
+                                </div>
+                            </td>
+                            <td>
+                                <div className="day">2</div>
+                                <div className="markers">
+                                    <div className="red marker"></div>
+                                    <div className="blue marker"></div>
+                                </div>
+                            </td>
+                            <td>
+                                <div className="day">3</div>
+                                <div className="markers">
+                                    <div className="red marker"></div>
+                                    <div className="blue marker"></div>
+                                </div>
+                            </td>
+                            <td>
+                                <div className="day">4</div>
+                                <div className="markers">
+                                    <div className="red marker"></div>
+                                    <div className="blue marker"></div>
+                                </div>
+                            </td>
+                            <td>
+                                <div className="day">5</div>
+                                <div className="markers">
+                                    <div className="red marker"></div>
+                                    <div className="blue marker"></div>
+                                </div>
+                            </td>
+                            <td>
+                                <div className="day">6</div>
+                                <div className="markers">
+                                    <div className="red marker"></div>
+                                    <div className="blue marker"></div>
+                                </div>
+                            </td>
+                            <td>
+                                <div className="day">7</div>
+                                <div className="markers">
+                                    <div className="red marker"></div>
+                                    <div className="blue marker"></div>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div className="foods">
+
+            </div>
+        </div>
+    )
+}

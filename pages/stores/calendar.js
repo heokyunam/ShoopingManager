1db6176fc1c7419490d76f535a2ddc1daf200a4b
heokyunam <heokyunam@naver.com>
2021-03-27 11:20:28 +0900

    mobx setDay 추가

--- pages/stores/calendar.js
+++ pages/stores/calendar.js
@@ -2,8 +2,12 @@ import {observable} from 'mobx';
 
 const calendar = observable({
     selectedDay: null,
+    days: [],
     setSelectedDay(day) {
         this.selectedDay = day;
+    },
+    setDays(days) {
+        this.days = days;
     }
 })
 
